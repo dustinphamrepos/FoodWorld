8d7707359a268291e915cea096fc03648a1c5cfa
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2024-08-21 15:38:00 +0700

    Update

--- foodWorld_main/settings.py
+++ foodWorld_main/settings.py
@@ -162,13 +162,17 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #   'foodWorld_main/static'
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'foodWorld_main/static'
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # settings.py
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.ManifestStaticFilesStorage'
 
 
 #Media Files
